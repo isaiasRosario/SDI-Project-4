781054982bd9966ba8050ae3ae0a2c14bcf4438f
r16902 <irosario@fullsail.edu>
2013-08-01 17:23:45 -0400

    Added new files to repository

---
+++ rosario_isaias_assignment4.js
@@ -0,0 +1,45 @@
+// Isaias Rosario
+// 7/25/13
+// Project 3
+// Code Library
+
+var myLib = function() {
+
+    // Check Phone Number
+	var checkPhNum = function(val) {
+	
+	
+	}; // end of checkPhNum
+	
+	
+	// Check Email Address
+	var checkEmail = funtion(val){
+	
+	
+	}; // end of checkEmail
+	
+	
+	return {
+		"checkPhNum" = checkPhNum
+		
+	}; // end return object
+	
+}; // end of myLib
+
+
+// Main Code
+
+var lib = new myLib()
+
+console.log("Is this a valid phone number " + lib.checkPhNum("413-333-3333"))
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
